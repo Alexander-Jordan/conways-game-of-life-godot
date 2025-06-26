9c0dae64ae55302365e24e80c0405f5637b5a1ef
Alexander-Jordan <curtjordan93@gmail.com>
2025-06-26 11:20:33 +0200

    Move everything related to the simulation to the SimulationManager, and clean up all code

--- utilities/mode_manager/mode_manager.gd
+++ utilities/mode_manager/mode_manager.gd
@@ -1,15 +1,23 @@
+## The global script for handling different user modes.
 class_name ModeManager extends Node
 
+#region ENUMS
+## Modes to choose from.
 enum Mode {
 	DEFAULT,
 	DRAW,
 	ERASE,
 }
+#endregion
 
+#region VARIABLES
+## The image of the custom arrow cursor.
 var cursor_arrow: Texture2D = preload('res://ui/cursors/cursor_arrow.png')
+## The image of the custom eraser cursor.
 var cursor_eraser: Texture2D = preload('res://ui/cursors/cursor_eraser.png')
+## The image of the custom pen cursor.
 var cursor_pen: Texture2D = preload('res://ui/cursors/cursor_pen.png')
-
+## The current mode.
 var mode: Mode = Mode.DEFAULT:
 	set(m):
 		if !Mode.values().has(m) or m == mode:
@@ -21,7 +29,9 @@ var mode: Mode = Mode.DEFAULT:
 			Input.set_custom_mouse_cursor(cursor_eraser, Input.CURSOR_ARROW, Vector2(0, 16))
 		else:
 			Input.set_custom_mouse_cursor(cursor_arrow)
+#endregion
 
+#region FUNCTIONS
 func _unhandled_input(event: InputEvent) -> void:
 	if event.is_action_pressed('mode_draw'):
 		mode = Mode.DRAW
@@ -29,3 +39,4 @@ func _unhandled_input(event: InputEvent) -> void:
 		mode = Mode.ERASE
 	if event.is_action_pressed('mode_default'):
 		mode = Mode.DEFAULT
+#endregion
