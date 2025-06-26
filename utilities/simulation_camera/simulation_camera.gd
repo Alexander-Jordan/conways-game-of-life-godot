85295d438f67fe43a9fc59c49f6fd60b80985488
Alexander-Jordan <curtjordan93@gmail.com>
2025-06-26 12:32:57 +0200

    Fix zooming out

--- utilities/simulation_camera/simulation_camera.gd
+++ utilities/simulation_camera/simulation_camera.gd
@@ -7,7 +7,7 @@ const ZOOM_STEP: float = 0.05
 func _unhandled_input(event: InputEvent) -> void:
 	if event.is_action_pressed('camera_zoom_out'):
 		zoom_out()
-	if event.is_action_pressed('camera_zoom_in'):
+	elif event.is_action_pressed('camera_zoom_in'):
 		zoom_in()
 
 func zoom_in() -> void:
